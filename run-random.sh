5792ac81c2520da321d1800a7cdc00f239afab5b
GianmarcoDIAG <121864914+GianmarcoDIAG@users.noreply.github.com>
2024-03-01 10:52:22 +0000

    Add files via upload

---
+++ run-random.sh
@@ -0,0 +1,19 @@
+cd Benchmarks/Random
+
+sudo chmod "u+x" exe-1-dominance.sh
+sudo chmod "u+x" exe-2-dominance.sh
+sudo chmod "u+x" exe-3-dominance.sh
+sudo chmod "u+x" exe-1-no-dominance.sh
+sudo chmod "u+x" exe-2-no-dominance.sh
+sudo chmod "u+x" exe-3-no-dominance.sh
+sudo chmod "u+x" exe-4-no-dominance.sh
+sudo chmod "u+x" exe-5-no-dominance.sh
+
+./exe-1-dominance.sh
+./exe-2-dominance.sh
+./exe-3-dominance.sh
+./exe-1-no-dominance.sh
+./exe-2-no-dominance.sh
+./exe-3-no-dominance.sh
+./exe-4-no-dominance.sh
+./exe-5-no-dominance.sh
